9e43e4dd9ae79e8866d5d363b7135bbcf724399a
KarthikaMovva <karthika.movva@kalvium.community>
2023-11-29 15:14:38 +0530

    added

--- src/app.js
+++ src/app.js
@@ -26,7 +26,16 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
+    for(let j=0; j<players.length; j++)  {
+        detailedPlayers.push({
+            name: players[j],
+            strength: getRandomStrength(),
+            img: "images/super-"+(j + 1)+".png",
+        
+        })
 
+    }
+      
     return detailedPlayers;
 }
 
@@ -34,6 +43,7 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.ceil(Math.random()*100)+1;
 }
 
 const buildPlayers = (players, type) => {
@@ -42,9 +52,19 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
-
+for(let j=0; j<players.length; j++) {
+        if ((type==='hero' && j%2===0) || (type=="villain" && j%2!==0)){
+            fragment+=`
+            <div class="player">
+             <img src="${players[j].img}">
+             <div class="name">${players[j].name}</div>
+             <div class="strength">${players[j].strength}</div>
+            </div>`;
+            
+        }
+    }
     return fragment;
-}
+};
 // Display players in HTML
 const viewPlayers = (players) => {
 
